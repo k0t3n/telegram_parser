efb2a5a66752ac1cec9d8f61e3ce43aed787713c
k0t3n <k0t3n.mail@gmail.com>
2017-12-29 22:36:02 +0300

    - Add channel info - Add settings

--- script.py
+++ script.py
@@ -1,1 +1,49 @@
-# script.py - main script file
+import sys
+from getpass import getpass
+from time import sleep
+from settings import *
+
+from telethon import TelegramClient
+from telethon.errors import SessionPasswordNeededError
+from telethon.errors.rpc_error_list import UsernameNotOccupiedError
+from telethon.errors.rpc_error_list import FloodWaitError
+from telethon.tl.functions.channels import GetParticipantsRequest
+from telethon.tl.functions.contacts import ResolveUsernameRequest
+from telethon.tl.types import ChannelParticipantsSearch, InputChannel
+
+
+def get_chat_info(username, client):
+    try:
+        chat = client(ResolveUsernameRequest(username))
+    except UsernameNotOccupiedError:
+        print('Chat/channel not found!')
+        sys.exit()
+    result = {
+        'chat_id': chat.peer.channel_id,
+        'access_hash': chat.chats[0].access_hash
+    }
+    return result
+
+
+def main():
+    client = TelegramClient('user-session', API_ID, API_HASH)
+    print('Connecting...')
+    client.connect()
+    if not client.is_user_authorized():
+        try:
+            client.send_code_request(PHONE)
+            print('Sending a code...')
+            client.sign_in(PHONE, code=input('Enter code: '))
+            print('Successfully!')
+        except FloodWaitError as FloodError:
+            print('Flood wait: {}.'.format(FloodError))
+            sys.exit()
+        except SessionPasswordNeededError:
+            client.sign_in(password=getpass('Enter password: '))
+            print('Successfully!')
+    print(get_chat_info(CHANNEL, client))
+    print('Done!')
+
+
+if __name__ == '__main__':
+    main()
